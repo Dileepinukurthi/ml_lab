9d4a89be6c7cd22a6b4b078bc332707a620fba87
HARSHA VARDHAN <75431678+thunder-007@users.noreply.github.com>
2022-12-11 22:57:48 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -8,7 +8,7 @@ def sample_analyze_sentiment(documents):
     text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     response = text_analytics_client.analyze_sentiment(documents, show_opinion_mining=True)
     results = [doc for doc in response if not doc.is_error]
-    for result in response:
+    for result in results:
         print(result.id, result.sentiment, result.confidence_scores)
 sample_analyze_sentiment([
         """I am good""",

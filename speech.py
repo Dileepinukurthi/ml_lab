6df86629ef797096b40abb6aed96bac631a7bd5b
HARSHA VARDHAN <75431678+thunder-007@users.noreply.github.com>
2022-12-11 22:55:04 +0530

    Create speech.py

---
+++ speech.py
@@ -0,0 +1,18 @@
+# pip install azure-ai-textanalytics
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+def sample_analyze_sentiment(documents):
+    endpoint = "" # Will be provided 
+    key = "" # Will be provided
+    text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+    response = text_analytics_client.analyze_sentiment(documents, show_opinion_mining=True)
+    results = [doc for doc in response if not doc.is_error]
+    for result in response:
+        print(result.id, result.sentiment, result.confidence_scores)
+sample_analyze_sentiment([
+        """I am good""",
+        """I am bad""",
+        """I am horny""",
+        """I am ok""",
+    ])
